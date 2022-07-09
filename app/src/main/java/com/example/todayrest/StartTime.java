8caa494d8f243f428f4de5e742aac9aeed7e4316
DDangDin <kcorelec7123@gmail.com>
2022-07-10 00:12:29 +0900

    시작시간,끝나는시간 처음에 체크하는 페이지

--- app/src/main/java/com/example/todayrest/StartTime.java
+++ app/src/main/java/com/example/todayrest/StartTime.java
@@ -6,13 +6,17 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class StartTime extends AppCompatActivity {
     TextView text_am;
     TextView text_pm;
     ImageButton btn_start_next;
+    EditText start_time_hour;
+    EditText start_time_minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,8 @@ public class StartTime extends AppCompatActivity {
         text_am = findViewById(R.id.text_Am);
         text_pm = findViewById(R.id.text_Pm);
         btn_start_next = findViewById(R.id.btn_start_next);
+        start_time_hour = findViewById(R.id.start_time_hour);
+        start_time_minute = findViewById(R.id.start_time_minute);
 
         text_am.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,7 +48,12 @@ public class StartTime extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(StartTime.this, EndTime.class);
-                startActivity(intent);
+                if(start_time_hour.getText().toString().equals("")
+                        || start_time_minute.getText().toString().equals("")){
+                    Toast.makeText(StartTime.this, "빈칸을 채워주세요", Toast.LENGTH_SHORT).show();
+                }else{
+                    startActivity(intent);
+                }
             }
         });
 
