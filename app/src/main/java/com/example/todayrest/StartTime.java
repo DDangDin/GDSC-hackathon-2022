8d5c177bae73213ed4e3027715fc1e11ff0f7f06
user <peach3139@naver.com>
2022-07-09 23:34:33 +0900

    다음 완료

---
+++ app/src/main/java/com/example/todayrest/StartTime.java
@@ -0,0 +1,50 @@
+package com.example.todayrest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class StartTime extends AppCompatActivity {
+    TextView text_am;
+    TextView text_pm;
+    ImageButton btn_start_next;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_time);
+
+        text_am = findViewById(R.id.text_Am);
+        text_pm = findViewById(R.id.text_Pm);
+        btn_start_next = findViewById(R.id.btn_start_next);
+
+        text_am.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text_am.setTextColor(Color.BLACK);
+                text_pm.setTextColor(Color.LTGRAY);
+            }
+        });
+
+        text_pm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text_pm.setTextColor(Color.BLACK);
+                text_am.setTextColor(Color.LTGRAY);
+            }
+        });
+        btn_start_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartTime.this, end_time.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/todayrest/end_time.java
@@ -0,0 +1,40 @@
+package com.example.todayrest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class end_time extends AppCompatActivity {
+    TextView text_am;
+    TextView text_pm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_time);
+
+        text_am = findViewById(R.id.text_Am);
+        text_pm = findViewById(R.id.text_Pm);
+
+        text_am.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text_am.setTextColor(Color.BLACK);
+                text_pm.setTextColor(Color.LTGRAY);
+            }
+        });
+
+        text_pm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text_pm.setTextColor(Color.BLACK);
+                text_am.setTextColor(Color.LTGRAY);
+            }
+        });
+
+    }
+
+}
